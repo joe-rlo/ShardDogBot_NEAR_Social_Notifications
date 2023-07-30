ed4699e5cc8729095852914a4a7e6ef49e9ca644
Joe - RLO | ShardDog <103778941+joe-rlo@users.noreply.github.com>
2023-07-29 22:14:00 -0400

    Cleanup sendmessage.py

--- src/sendmessage.py
+++ src/sendmessage.py
@@ -1,6 +1,6 @@
 import pip._vendor.requests 
 
-url = "https://api.telegram.org/botID:APIKEY/sendMessage?parse_mode=html&chat_id=5222409941&text=<i>Test</i> From NEAR Social!"
+url = "https://api.telegram.org/APIKEY/sendMessage?parse_mode=html&chat_id=5222409941&text=<i>Test</i> From NEAR Social!"
 
 payload={}
 headers = {}
